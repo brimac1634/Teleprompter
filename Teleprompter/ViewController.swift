cbf66ce7fdcf0120d91e0060e343b20a786a0257
Brian MacPherson <brimac1634@gmail.com>
2018-11-21 13:48:26 +0800

    Initial Commit

---
+++ Teleprompter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Teleprompter
+//
+//  Created by Brian MacPherson on 21/11/2018.
+//  Copyright © 2018 Brian MacPherson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
