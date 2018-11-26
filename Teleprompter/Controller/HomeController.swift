b887e5e2e8e298c699e85814288d9a25b3b4fa4f
Brian MacPherson <brimac1634@gmail.com>
2018-11-26 17:52:01 +0800

    Fixed optional of first page

--- Teleprompter/Controller/HomeController.swift
+++ Teleprompter/Controller/HomeController.swift
@@ -116,7 +116,8 @@ class HomeController: UIViewController, UIDocumentPickerDelegate {
     @objc func handleStart() {
         if textBox.text?.count != 0 {
             let rollingTextController = RollingTextController()
-            rollingTextController.textInput = "\n\n\n\n\(textBox.text)\n\n\n\n\n"
+            guard let text = textBox.text else {return}
+            rollingTextController.textInput = "\n\n\n\n\(text)\n\n\n\n\n"
             rollingTextController.view.backgroundColor = .black
             navigationController?.isNavigationBarHidden = true
             navigationController?.pushViewController(rollingTextController, animated: true)
